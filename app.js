6a35335eb5a794ebec94090f0de8744720dd05e6
Justas Gudonis <gudonisjustas@gmail.com>
2022-04-07 22:27:21 +0300

    completed 9 out of 21 steps of the challange

--- app.js
+++ app.js
@@ -10,11 +10,33 @@ const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
+const pages = ["Home", "About", "Contact", "Compose"];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.render("home", { pageTitle: pages[0], content: homeStartingContent });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { pageTitle: pages[1], content: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { pageTitle: pages[2], content: contactContent });
+});
+
+app.get("/compose", function (req, res) {
+  res.render("compose", { pageTitle: pages[3] });
+});
+
+app.post("/compose", function (req, res) {
+  const title = req.body.postTitle;
+  console.log(title);
+  res.redirect("/compose");
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
